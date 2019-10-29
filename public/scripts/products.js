a2b904f2ecfa9d4ab058f1693ecb2495519e07dd
vmarinov <vmarinovrs@gmail.com>
2019-10-29 13:58:59 +0200

    changes to products script

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,10 +1,16 @@
-const productPrice = $('input[name="unconvertedPrice"]').val();
-const priceElem = $('span[name="price"]');
-function convertPrice(rate) {
-  if (rate == 0) {
-    priceElem.text(`Price: ${productPrice}`);
-    $('<p style="color:red;"> Unable to convert price </p>').insertAfter(priceElem);
-  } else {
-    priceElem.text(`Price: ${(productPrice / rate).toFixed(2)}`);
-  }
-}
+$(document).ready(() => {
+  const currenciesSelector = $('select[name="currencies"]');
+  const priceElem = $('span[name="price"]');
+  const productPrice = $('input[name="unconvertedPrice"]').val();
+
+  currenciesSelector.on('change', () => {
+    priceElem.next('p').remove();
+    const rate = currenciesSelector.val();
+    if (rate == 0) {
+      priceElem.text(`Price: ${productPrice}`);
+      $('<p style="color:red;"> Unable to convert price </p>').insertAfter(priceElem);
+    } else {
+      priceElem.text(`Price: ${(productPrice / rate).toFixed(2)}`);
+    }
+  });
+});
